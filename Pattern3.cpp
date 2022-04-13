6fde523ac4c7566a65b4f71a6a9de54d1b5aa527
Varun Tandon <75672315+Mrtandon20@users.noreply.github.com>
2022-04-13 23:22:39 +0530

    Pattern 3
    ****
    ***
    **
    *

---
+++ Pattern3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the Value of N : ";
+    cin>>n;
+
+    int i=n;
+    while(i>=1){
+        int j=1;
+        while(j<=i){
+            cout<<"*";
+            j++;
+        }
+        i--;
+        cout<<endl;
+    }
+}
